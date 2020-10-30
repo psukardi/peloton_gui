136e0d1f7f34792d427457b272ee9751caca6df6
Bryan Kardisco <bryan.kardisco@gmail.com>
2020-10-29 21:57:11 -0400

    I think this is right

---
+++ src/pages/Dashboard/UserTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <base-table :data="table.data"
+              :columns="table.columns"
+              thead-classes="text-primary">
+  </base-table>
+</template>
+<script>
+  import { BaseTable } from "@/components";
+  import axios from "axios";
+  export default {
+    components: {
+      BaseTable
+    },
+    computed: {
+      table(){
+        return this.$t('dashboard.usersTable');
+      }
+    },
+    mounted() {
+      // once this is loaded we can pump in the course data
+      async function asyncFunc(table) {
+          const [firstResponse] = await Promise.all([
+            axios.get('http://54.90.15.230:5000/course_data'),
+          ]);
+
+          table.data = firstResponse.data
+      }
+
+      asyncFunc(this.$t('dashboard.usersTable'));
+    },
+  }
+</script>
+<style>
+</style>
