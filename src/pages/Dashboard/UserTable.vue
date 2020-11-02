3e84e9725d77e90245c18dae16f12224cc1da400
Bryan Kardisco <bryan.kardisco@gmail.com>
2020-11-02 15:10:55 -0500

    ROLLBACK

--- src/pages/Dashboard/UserTable.vue
+++ src/pages/Dashboard/UserTable.vue
@@ -11,35 +11,22 @@
     components: {
       BaseTable
     },
-    methods:{
-      getCookieValue(a) {
-          var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
-          return b ? b.pop() : 'f9982d7545db41be91e2fff28000547d';
-      }
-    },
     computed: {
       table(){
         return this.$t('dashboard.usersTable');
       }
     },
     mounted() {
-        async function asyncFunc(table, getCookieValue){
-          var tmp_user_id = getCookieValue("USER_ID")
-          var url = 'http://pelodashboard.com:5000/course_data/' + tmp_user_id;
+      // once this is loaded we can pump in the course data
+      async function asyncFunc(table) {
           const [firstResponse] = await Promise.all([
-              axios({
-                method: 'get',
-                url: url,
-              }).catch(function (error){
-                alert(error);
-                alert(JSON.stringify(error));
-              })
+            axios.get('http://pelodashboard.com:5000/course_data'),
           ]);
 
           table.data = firstResponse.data
-        }
+      }
 
-        asyncFunc(this.$t('dashboard.usersTable'), this.getCookieValue);
+      asyncFunc(this.$t('dashboard.usersTable'));
     },
   }
 </script>
