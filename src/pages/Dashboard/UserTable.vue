b5b700a278d02366ad5ddf9e3285fc47033a4bb8
Bryan Kardisco <bryan.kardisco@gmail.com>
2020-11-02 02:13:43 -0500

    Cookies

--- src/pages/Dashboard/UserTable.vue
+++ src/pages/Dashboard/UserTable.vue
@@ -15,6 +15,7 @@
       getCookieValue(a) {
           var b = document.cookie.match('(^|;)\\s*' + a + '\\s*=\\s*([^;]+)');
           return b ? b.pop() : '';
+      }
     },
     computed: {
       table(){
